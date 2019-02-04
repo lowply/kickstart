a8d2dcd5c65dee91c77b8a5c243448829db20a48
Sho Mizutani <lowply@github.com>
2019-02-03 23:23:21 -0800

    Add time and date adjustment

--- amazon-linux-2/00_system.sh
+++ amazon-linux-2/00_system.sh
@@ -6,3 +6,6 @@ echo "Change SSH port"
 sed -i 's/#Port 22/Port 1417/' /etc/ssh/sshd_config
 systemctl restart sshd
 systemctl enable sshd
+
+echo "Configure date"
+timedatectl set-timezone Asia/Tokyo
