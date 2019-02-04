90a1a3279a1e3b189273e077b50fb7ed8943426b
Sho Mizutani <lowply@github.com>
2019-02-03 22:34:41 -0800

    Use extra repository for vim

--- amazon-linux-2/04_vim.sh
+++ amazon-linux-2/04_vim.sh
@@ -2,27 +2,5 @@
 
 . ./lib
 
-VERSION="8.1.0873"
-
 echo "Install vim"
-yum -y remove vim-common vim-enhanced
-yum -y install python3 python3-devel lua lua-devel autoconf gcc xmlto asciidoc docbook2X curl curl-devel perl perl-devel expat expat-devel gettext openssl openssl-devel ncurses-devel
-
-cd /usr/local/src/
-curl -OL https://github.com/vim/vim/archive/v8.1.0873.tar.gz
-
-tar vxzf v${VERSION}.tar.gz
-cd vim-${VERSION}
-
-./configure \
-    --prefix=/usr/local/vim \
-    --with-features=huge \
-    --enable-multibyte \
-    --enable-luainterp \
-    --enable-fail-if-missing \
-    --enable-python3interp
-
-make && make install
-
-cd /usr/local/bin
-ln -s /usr/local/vim/bin/* .
+amazon-linux-extras install vim
--- amazon-linux-2/install.sh
+++ amazon-linux-2/install.sh
@@ -2,11 +2,9 @@
 
 ./00_system.sh
 ./01_yum.sh
-./02_git.sh
+./02_go.sh
 ./03_user.sh
-./04_python3.sh
-./05_go.sh
-./06_neovim.sh
+./04_vim.sh
 ./07_tmux.sh
 ./08_nginx.sh
 ./09_docker.sh
